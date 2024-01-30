7ea40d65a46e6dc9f9c332da1a89a26210332568
iphonesiri1996 <navjyotabhale0105@gmail.com>
2024-01-30 11:00:41 +0530

    new code

---
+++ transale.py
@@ -0,0 +1,12 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-hs6j037urx6iav+7#10%-vu4l4f5@@-1_zo)oft4g7$vf2$jmp'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
